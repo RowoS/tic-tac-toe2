d1be8e8a7d7751abee9fdf2b2a72420e7902dc7d
Lejohn Owen A. Loreto <23-1-00191@vsu.edu.ph>
2025-09-17 15:03:01 +0800

    Add tic-tac-toe components
    Creates components for tic-tac-toe and apply it to App file

---
+++ src/components/Square.tsx
@@ -0,0 +1,9 @@
+import type { SquareProps } from "../types/types";
+
+export function Square({ value, onSquareClick }: SquareProps) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
